d98e4cd4507ba9bcb33a85d0f9943e926d85dde4
Jordy <167139696+JordyChamba@users.noreply.github.com>
2024-05-07 10:12:52 -0500

    Update Student.java

--- Proyecto_003/src/default_package/Student.java
+++ Proyecto_003/src/default_package/Student.java
@@ -1,15 +1,13 @@
 package default_package;
 
 public class Student {
-	int id;
 	String name;
 	String lastName;
 	int age;
 	
-	public Student(int id, String name, String lastName, int age) {
+	public Student( String name, String lastName, int age) {
 		// TODO Auto-generated constructor stub
 		this.age=age;
-		this.id=id;
 		this.lastName=lastName;
 		this.name=name;
 		
@@ -22,21 +20,12 @@ public class Student {
 	@Override
 	public String toString() {
 	    return "Student{" +
-	            "ide=" + id +
 	            ", name='" + name + '\'' +
 	            ", lastName='" + lastName + '\'' +
 	            ", age=" + age +
 	            '}';
 	}
 	
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
 
 	public String getName() {
 		return name;
